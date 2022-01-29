8220a3fcc0d4958301118a2d94b8d79ecb917586
SergSukh <s9626265386@gmail.com>
2022-01-29 08:32:26 +0400

    1

--- yatube/posts/tests/test_forms.py
+++ yatube/posts/tests/test_forms.py
@@ -1,5 +1,10 @@
+import shutil
+import tempfile
+
 from django.shortcuts import get_object_or_404
-from django.test import Client, TestCase
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.test import Client, TestCase, override_settings
+from django.conf import settings
 from django.urls import reverse
 from django.contrib.auth import get_user_model
 
@@ -8,7 +13,10 @@ from .fixtures.constant_post import test_post_text, test_group
 
 User = get_user_model()
 
+TEMP_MEDIA_ROOT = tempfile.mkdtemp(dir=settings.BASE_DIR)
 
+
+@override_settings(MEDIA_ROOT=TEMP_MEDIA_ROOT)
 class PostFormTests(TestCase):
     @classmethod
     def setUpClass(cls):
@@ -24,6 +32,11 @@ class PostFormTests(TestCase):
             text=test_post_text,
             group=cls.group,
         )
+
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+        shutil.rmtree(TEMP_MEDIA_ROOT, ignore_errors=True)
 
     def setUp(self):
         self.guest = Client()
@@ -83,3 +96,38 @@ class PostFormTests(TestCase):
         self.assertEqual(post_count, self.post.pk)
         self.assertEqual(post.group, self.post.group)
         self.assertNotEqual(post.text, post_old.text)
+
+    def test_creat_post_image_content(self):
+        post_count = Post.objects.count()
+        small_gif = (
+            b'\x47\x49\x46\x38\x39\x61\x01\x00'
+            b'\x01\x00\x00\x00\x00\x21\xf9\x04'
+            b'\x01\x0a\x00\x01\x00\x2c\x00\x00'
+            b'\x00\x00\x01\x00\x01\x00\x00\x02'
+            b'\x02\x4c\x01\x00\x3b'
+        )
+        uploaded = SimpleUploadedFile(
+            name='small.gif',
+            content=small_gif,
+            content_type='image/gif'
+        )
+        form_data = {
+            'text': 'Тестовый текст3',
+            'group': f'{self.group.pk}',
+            'image': uploaded,
+        }
+        response = self.auth_client.post(
+            reverse('posts:post_create'),
+            data=form_data,
+            follow=True
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(Post.objects.count(), post_count + 1)
+        self.assertTrue(
+            Post.objects.filter(
+                text='Тестовый текст3',
+                author=self.user,
+                group=self.group.pk,
+                #image='posts/small.gif'
+                ).exists()
+        )
--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -1,8 +1,13 @@
+import shutil
+import tempfile
+
 from datetime import datetime
-from random import random
 
 from django.contrib.auth import get_user_model
-from django.test import Client, TestCase
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.http import response
+from django.test import Client, TestCase, override_settings
+from django.conf import settings
 from django.urls import reverse
 
 from posts.models import Group, Post
@@ -11,7 +16,10 @@ from .fixtures import constant_post
 
 User = get_user_model()
 
+TEMP_MEDIA_ROOT = tempfile.mkdtemp(dir=settings.BASE_DIR)
 
+
+@override_settings(MEDIA_ROOT=TEMP_MEDIA_ROOT)
 class PostViewTests(TestCase):
     @classmethod
     def setUpClass(cls):
@@ -22,9 +30,22 @@ class PostViewTests(TestCase):
             slug=constant_post.test_group['slug'],
             description=constant_post.test_group['description'],
         )
+        small_gif = (
+            b'\x47\x49\x46\x38\x39\x61\x01\x00'
+            b'\x01\x00\x00\x00\x00\x21\xf9\x04'
+            b'\x01\x0a\x00\x01\x00\x2c\x00\x00'
+            b'\x00\x00\x01\x00\x01\x00\x00\x02'
+            b'\x02\x4c\x01\x00\x3b'
+        )
+        uploaded = SimpleUploadedFile(
+            name='small.gif',
+            content=small_gif,
+            content_type='image/gif'
+        )
         post = (Post(pk=i,
                      author=cls.user,
                      group=cls.group,
+                     image=uploaded,
                      text=f'Тестовый текст{i}',
                      pub_date=datetime(2017, 1, i)
                      ) for i in range(1, 13)
@@ -51,6 +72,11 @@ class PostViewTests(TestCase):
                     kwargs={'slug': f'{cls.post[0].group}'}),
         }
 
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+        shutil.rmtree(TEMP_MEDIA_ROOT, ignore_errors=True)
+
     def setUp(self):
         self.client = Client()
         self.client.force_login(self.user)
@@ -72,10 +98,16 @@ class PostViewTests(TestCase):
         for template, reverse_name in templates_pages_names.items():
             with self.subTest(reverse_name=reverse_name):
                 response = self.client.get(reverse_name)
-                rnd = Post.objects.count() - int(random() * 10)
-                post_case = Post.objects.get(id=rnd)
+                post_case = Post.objects.first()
                 form_field = response.context.get('page_obj')
                 self.assertIn(post_case, form_field)
+    
+    def test_post_detail_pages_show_correct_context(self):
+        response = self.client.get(reverse('posts:post_detail', kwargs={'post_id': f'{self.post[0].pk}'}))
+        self.assertEqual(response.context['post'].text[:14], 'Тестовый текст' )
+        self.assertEqual(response.context['post'].group, self.group )
+        self.assertEqual(response.context['post'].author, self.user )
+        self.assertEqual(response.context['post'].image, 'posts/small.gif' )
 
     def test_first_page_contains_ten_records(self):
         templates_pages_names = self.paginator_pages_names
